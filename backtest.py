81c8069d847f8918a514c2256fb2cdf65dca1f31
Arseniy Batyrov <a.batyrov@corp.badoo.com>
2017-11-02 18:49:18 +0300

    renamed API, added sell

--- backtest.py
+++ backtest.py
@@ -2,7 +2,7 @@ import sys, getopt
 
 from botChart import BotChart
 from botStrategy import BotStrategy
-from botApi import BotApi
+from poloApi import BotApi
 
 def main(argv):
     chart = BotChart("poloniex", "BTC_BCH", 300)
@@ -10,6 +10,10 @@ def main(argv):
     strategy = BotStrategy()
     for candlestick in chart.getPoints():
         strategy.tick(candlestick)
+
+    api = BotApi
+
+    api.buy(BotApi, pair, rate, amount)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- poloApi.py
+++ poloApi.py
@@ -6,8 +6,8 @@ class BotApi(object):
     polo.secret = 'd51d59524afdebe60c5d19df7cbdc592446ff15fabb97c4538165b051d14654828ae670e5c6f21dd0ea1cc94a444bf2a9ac2a82138021f2f83e16abd6408c0f7'
 
     def buy (self, pair, rate, amount):
-        if amount > 0.0001:
-            amount = 0.0001
         polo.buy(pair, rate, amount)
 
-    polo.buy("BTC_BCH", 0.07662897, 0.002)
+    def sell (self, pair, rate, amount):
+        polo.buy(pair, rate, amount)
+
