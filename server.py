263d5a82b92706d6b061a15a7a05318fc15287c0
Arseny Batyrov <comolder@gmail.com>
2017-11-16 19:29:21 +0300

    Fixed server showing

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ from os import environ
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.run(environ.get('5000'))
+# app.run(environ.get('5000'))
 
 @app.route("/")
 def main():
+    # return "Welcome"
     return render_template('index.html')
 
 if __name__ == "__main__":
