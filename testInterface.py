e55802ad8d814a422e27b829221519d26308d3d3
Arseny Batyrov <comolder@gmail.com>
2017-11-16 18:53:15 +0300

    test

--- testInterface.py
+++ testInterface.py
@@ -1,17 +1,17 @@
-from poloApi import PoloApi
-from strategy import Strategy
-from ranking import Ranking
-from datafiles import DataFiles
-from database import Database
-api = PoloApi()
-strategy = Strategy()
-rank = Ranking()
-file = DataFiles()
-db = Database()
+# from poloApi import PoloApi
+# from strategy import Strategy
+# from ranking import Ranking
+# from datafiles import DataFiles
+# from database import Database
+# api = PoloApi()
+# strategy = Strategy()
+# rank = Ranking()
+# file = DataFiles()
+# db = Database()
 
 
 class TestInterface(object):
     def __init__(self):
         pass
 
-    strategy.profitSell()
+    # strategy.profitSell()
