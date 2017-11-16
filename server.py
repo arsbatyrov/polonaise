0bf8c19e3ead32da497805008e22a4a700a6b397
Arseny Batyrov <comolder@gmail.com>
2017-11-16 19:43:19 +0300

    Added port for heroku

--- server.py
+++ server.py
@@ -2,12 +2,10 @@ from os import environ
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# app.run(environ.get('5000'))
 
 @app.route("/")
 def main():
-    # return "Welcome"
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(environ.get('PORT'))
